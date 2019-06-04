b429d8d3a3b8f1f3f73592fe17e84b59d8858927
Madeline Jones <brandkast@Madelines-MBP.hsd1.fl.comcast.net>
2019-06-04 19:39:28 -0400

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Expense extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expense: null,
+      description: null
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+      // This is the syntax for changing the object's values
+    });
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.props.addItem}>
+            <input
+              type="text"
+              name="expense"
+              value={this.state.expense}
+              placeholder="Expense"
+              onChange={this.handleInputChange}
+            />
+            <input
+              type="text"
+              name="description"
+              value={this.state.description}
+              placeholder="Description"
+              onChange={this.handleInputChange}
+            />
+            <button type="submit" value="Add" />
+        </form>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Expense />
+        {/* <ExpenseList entries={this.state.expense} /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
